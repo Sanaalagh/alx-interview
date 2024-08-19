1e651e12c8eb2684d095d6c58dbe8be254cdfbfc
sanaalagh <sanaalaghzou@gmail.com>
2024-08-19 19:21:48 +0000

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,18 +4,18 @@ const request = require('request');
 const movieId = parseInt(process.argv[2], 10);
 
 request.get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, async (error, response, body) => {
-  if (error) return;
-  const movie = JSON.parse(body);
+	  if (error) return;
+	  const movie = JSON.parse(body);
 
-  for (const character of movie.characters) {
-    const characterName = await new Promise((resolve, reject) => {
-      request.get(character, (error, response, body) => {
-        if (error) return;
-        const characterInfo = JSON.parse(body);
-        resolve(characterInfo.name);
-      });
-    });
-    console.log(characterName);
-  }
+	  for (const character of movie.characters) {
+		      const characterName = await new Promise((resolve, reject) => {
+			            request.get(character, (error, response, body) => {
+					            if (error) return;
+					            const characterInfo = JSON.parse(body);
+					            resolve(characterInfo.name);
+					          });
+			          });
+		      console.log(characterName);
+		    }
 });
 
